18ba2c78b44892d2d5539625cb5b59a790b73a82
1616imuimu <yagasaki.kei@gmail.com>
2023-02-24 02:26:46 +0900

    p89

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,27 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { graphql } from "gatsby"
 
-const IndexPage = () => {
+const IndexPage = props => {
   return (
     <div>
       <p> Hello Gatsby </p>
       <Link to="/about">Link to About</Link>
+      <pre>{JSON.stringify(props.data, null, 2)}</pre>
     </div>
   )
 }
+
+export const query = graphql`
+  query IndexPageQuery {
+    allFeedQiita {
+      nodes {
+        title
+        link
+        pubDate
+      }
+    }
+  }
+`
 
 export default IndexPage
