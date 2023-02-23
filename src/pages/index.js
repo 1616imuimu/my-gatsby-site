168d1cac12c61351c14676d4c0bd86b545f5a65b
1616imuimu <yagasaki.kei@gmail.com>
2023-02-24 02:11:41 +0900

    p76

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,13 @@
 import * as React from "react"
+import { Link } from "gatsby"
 
 const IndexPage = () => {
-  return <p> Hello Gatsby </p>
+  return (
+    <div>
+      <p> Hello Gatsby </p>
+      <Link to="/about">Link to About</Link>
+    </div>
+  )
 }
 
 export default IndexPage
